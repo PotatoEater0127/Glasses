e08a35cedc185feb8b645ca4ec9f6206622f5783
Henry Han <potatoeater0127@gmail.com>
2020-06-05 16:01:30 +0800

    (fix) checkbox size on mobile

--- asset/js/effect.js
+++ asset/js/effect.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  console.log("ready");
   $(".ham-icon").click(function (e) {
-    console.log(e);
     e.preventDefault();
     $(".header__list").toggleClass("active");
   });
